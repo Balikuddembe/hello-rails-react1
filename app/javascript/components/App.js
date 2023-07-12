2e6227752d952fcf27667cb8d899cde09e62f6e0
Balikuddembe <balixemma@gmail.com>
2023-07-12 11:18:16 +0300

    Setup hello world project with webpack

---
+++ app/javascript/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+import Greeting from "./Greeting";
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Greeting />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
